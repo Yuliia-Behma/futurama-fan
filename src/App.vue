b5a5c6e93b6561da5675bb2201b8f99ce67956d9
Juliia <juliyabegma@gmail.com>
2024-06-17 23:12:16 +0300

    Add header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div class="navigation-container">
     <div class="logo-box">
-      logo
-      <h1></h1>
+      <img src="~@/assets/img/Logo-Futurama.svg" alt="logo">
+      <h2>Футурама фан</h2>
     </div>
     <nav></nav>
   </div>
+  <Header id="header"></Header>
   
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: futurama-bold;
+  src: url('~@/assets/fonts/Futurama Bold Font.ttf');
+}
+@font-face {
+  font-family: futura-medium;
+  src: url('~@/assets/fonts/Futura-font-Medium-Cyrillic.otf');
+}
+@font-face {
+  font-family: futurama-title;
+  src: url('~@/assets/fonts/Futurama Title Font.ttf');
+}
 
 body{
-  background-color: rgb(30, 30, 30);
+  background-color: #1e1e1e;
   margin: 0;
+  width: 100%;
+}
+
+.navigation-container{
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%);
+  padding: 12px 16px;
+  z-index: 10;
+  background-color: #0C1B43CC;
+  display: flex;
+}
+.logo-box{
+  display: flex;
+  flex-direction: row;
+}
+.logo-box > h2{
+  margin: 0;
+  color: #A4CAFE;
+font-family: futurama-bold;
+font-size: 24px;
+font-weight: 700;
+line-height: 150%;
+letter-spacing: 0%;
+text-align: left;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<script setup></script>
+
+<template>
+  <div class="header-container">
+    <h1 class="main-heading">Футурама</h1>
+    <button class="adventure-btn" data-augmented-ui="tl-clip br-clip">
+      Пригоди чекають
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.header-container {
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(
+      180deg,
+      rgba(12, 27, 67, 0) 80%,
+      rgb(12, 27, 67) 100%
+    ),
+    url("../assets/img/Picture_back.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  background-position: center;
+}
+
+.adventure-btn {
+  position: absolute;
+  bottom: 80px;
+  left: 50%;
+  transform: translate(-50%);
+  width: 253px;
+  height: 64px;
+  border: none;
+  background-color: #e3a008;
+  color: #0c1b43;
+  font-family: futurama-bold;
+  /* font-family: Futurama Bold Font(RUS BY LYAJK; */
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: 8%;
+  text-align: center;
+}
+
+.main-heading {
+  position: absolute;
+  margin: 0;
+  top: 120px;
+  left: 50%;
+  transform: translate(-50%);
+
+  background-image: linear-gradient(180deg, rgb(239, 30, 42), rgb(177, 41, 0));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  font-family: futura-medium;
+  font-weight: 400;
+  line-height: 68px;
+  letter-spacing: 0.08em;
+  text-align: center;
+  text-transform: uppercase;
+  filter: drop-shadow(4px 4px 0 #000);
+  -webkit-text-stroke: 1.5px #fee472;
+  font-size: 96px;
+}
+</style>
