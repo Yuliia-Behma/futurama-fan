e223913bbfba99639805e753c637b03c45215489
Juliia <juliyabegma@gmail.com>
2024-07-21 16:21:27 +0300

    work with tabs menu

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/styles/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/styles/main.css";
+import { Tabs, Tab } from "vue3-tabs-component";
 
-createApp(App).mount('#app')
+createApp(App).component("tabs", Tabs).component("tab", Tab).mount("#app");
--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -11,8 +11,21 @@
       src="../assets/img/icons3D/photo-camera-icon-1.svg"
       alt="camera"
     />
-    
+    <tabs>
+      <tab name="Усі">
+        <h2>Lorem ipsum dolor sit amet.</h2>
+      </tab>
+      <tab name="Фрай"> Hello Fri </tab>
+      <tab name="Ліла"> I am Leela\ </tab>
+      <tab name="Бендер"></tab>
+      <tab name="Емі"></tab>
+      <tab name="Гермес"></tab>
+      <tab name="Професор Фарнсворт"></tab>
+      <tab name="Джон Зойдберг"></tab>
+    </tabs>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
