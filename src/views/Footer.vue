d04f1798bd622bb138786eca6866751d9570c3c5
Juliia <juliyabegma@gmail.com>
2024-08-18 16:30:15 +0300

    Footer in progress

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -21,13 +21,116 @@
         />
       </div>
     </div>
-    <div></div>
+    <div class="footer-container">
+      <div class="logo-container">
+        <img src="../assets/img/leading-icon.svg" alt="logo" />
+        <span class="site-name">Футурама фан</span>
+      </div>
+      <nav class="nav-links">
+        <a href="#introduction">Персонажi</a>
+        <a href="">Трейлери</a>
+        <a href="#gallery">Галерея</a>
+        <a href="#cast">Актори</a>
+        <a href="">Сезони</a>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M20 10.6667C20 15.4532 15.9706 19.3333 11 19.3333C6.02943 19.3333 2 15.4532 2 10.6667C2 5.8802 6.02943 2 11 2C15.9706 2 20 5.8802 20 10.6667Z"
+            fill="#111928"
+          />
+          <path
+            d="M17.3334 17.3335L22 22.0002"
+            stroke="currentColor"
+            stroke-width="3"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M18.5 10.6667C18.5 14.5719 15.196 17.8333 11 17.8333C6.804 17.8333 3.5 14.5719 3.5 10.6667C3.5 6.76148 6.804 3.5 11 3.5C15.196 3.5 18.5 6.76148 18.5 10.6667Z"
+            stroke="currentColor"
+            stroke-width="3"
+          />
+        </svg>
+      </nav>
+      <div class="decor">
+        <svg
+          width="1920"
+          height="54"
+          viewBox="0 0 1920 54"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="240" y="8" width="541" height="2" fill="#1E429F" />
+          <path
+            d="M921.938 16L916.938 11H910.938L915.938 16H921.938Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M910.938 16L905.938 11H899.938L904.938 16H910.938Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M1000.94 16L1005.94 11H1011.94L1006.94 16H1000.94Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M1011.94 16L1016.94 11H1022.94L1017.94 16H1011.94Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M799.898 5.7752H799.965L799.898 5.70959V5.7752Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M895.981 4.36594C896.944 4.36594 897.725 3.75793 897.725 3.00837C897.725 2.2588 896.944 1.65088 895.981 1.65088C895.017 1.65088 894.236 2.2588 894.236 3.00837C894.236 3.75793 895.017 4.36594 895.981 4.36594Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M1122.19 5.48181L1122.12 5.54742H1122.19V5.48181Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M1026.1 4.45944C1027.07 4.45944 1027.85 3.85144 1027.85 3.10187C1027.85 2.3523 1027.07 1.74438 1026.1 1.74438C1025.14 1.74438 1024.36 2.3523 1024.36 3.10187C1024.36 3.85144 1025.14 4.45944 1026.1 4.45944Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M964 7C964 9.20914 962.209 11 960 11C957.791 11 956 9.20914 956 7C956 4.79086 957.791 3 960 3C962.209 3 964 4.79086 964 7Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M956.126 5.99998L949 6V3.97928H942V6H888L882 0H850.853V0.0207456L799 0L805 5H819L822 8H781V10H824L829 16H899L894 11H869L861 2H881L887 8H942V10H949V8H956.126C956.044 7.68038 956 7.3453 956 7C956 6.65469 956.044 6.31961 956.126 5.99998Z"
+            fill="#1E429F"
+          />
+          <path
+            d="M1139 8H1099L1102 5H1116L1122 0H1039L1033 6H979V4H972V6H963.874C963.956 6.31962 964 6.6547 964 7C964 7.33412 963.959 7.65869 963.882 7.96892H972V10H979V8H1034L1040 2H1060L1051 11H1026L1022 16H1092L1097 10H1139V8Z"
+            fill="#1E429F"
+          />
+          <rect x="1139" y="8" width="541" height="2" fill="#1E429F" />
+          <path
+            d="M0 28H642L676 53H1241L1275 28H1920"
+            stroke="#1E429F"
+            stroke-width="2"
+          />
+        </svg>
+      </div>
+      <div class="bottom-section">
+        <div class="follow-us"></div>
+        <img src="" alt="" />
+        <div class="license"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .wrapper {
   width: 100vw;
+  overflow: hidden;
 }
 
 .top-container {
@@ -35,13 +138,75 @@
   background-color: #000d1c;
 }
 
-.left-position{
-    left: -310px;
-    top: 69px;
+.left-position {
+  left: -310px;
+  top: 69px;
 }
 
-.right-position{
-    top: 69px;
-    right: -310px;
+.right-position {
+  top: 69px;
+  right: -310px;
+}
+
+.footer-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #0c1b43bf;
+  padding: 32px 0;
+}
+
+.logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 32px;
+}
+
+.site-name {
+  font-family: futurama-bold;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 150%;
+  color: #a4cafe;
+}
+
+.nav-links {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+}
+
+.nav-links a {
+  text-decoration: none;
+  padding: 10px 16px;
+  font-family: futurama-bold;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 150%;
+  letter-spacing: 0.08em;
+  color: #3f83f8;
+}
+
+.nav-links a:hover {
+  color: #a4cafe;
+  transition: all 0.3s cubic-bezier(0.23, 1, 0.32, 1);
+}
+
+.nav-links svg {
+  color: #3f83f8;
+}
+
+.nav-links svg:hover {
+  color: #a4cafe;
+  transition: all 0.3s cubic-bezier(0.23, 1, 0.32, 1);
+}
+
+.decor {
+  width: 100%;
+  overflow: hidden;
+  margin-top: 56px;
 }
 </style>
